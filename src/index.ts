4586e16bb4b5576772d96721537378dbad4e917c
Henrique Monteiro <henriquemonteiro942@gmail.com>
2022-04-29 14:44:27 -0300

    swagger adicionado e configurado

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@
 /* eslint-disable import-helpers/order-imports */
 /* eslint-disable prettier/prettier */
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFileConfig from "./swagger.json";
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFileConfig))
 
 app.use("/users", usersRoutes);
 
